9831a76ce7eeb6285c4104095b5d02c2fe7ea2ba
YINanZaZheng <614843497@qq.com>
2020-04-17 11:25:55 +0800

    测试git1

--- src/main/java/com/example/demo1/interceptor/LogInterceptor.java
+++ src/main/java/com/example/demo1/interceptor/LogInterceptor.java
@@ -2,9 +2,29 @@ package com.example.demo1.interceptor;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class LogInterceptor {
- //  十大
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogInterceptor extends HandlerInterceptorAdapter {
     //准写代码了，能提交吗
     private static final Logger logger= LoggerFactory.getLogger(LogInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("=======LogInterceptor.preHandle===============");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        super.afterCompletion(request, response, handler, ex);
+    }
 }
