115cb76ce362c943481b6a1c5a1bb176d0de9bce
YINanZaZheng <614843497@qq.com>
2020-04-17 11:56:17 +0800

    测试git2

--- src/main/java/com/example/demo1/interceptor/LogInterceptor.java
+++ src/main/java/com/example/demo1/interceptor/LogInterceptor.java
@@ -12,19 +12,22 @@ public class LogInterceptor extends HandlerInterceptorAdapter {
     //准写代码了，能提交吗
     private static final Logger logger= LoggerFactory.getLogger(LogInterceptor.class);
 
+    //前置处理，进行编码、安全控制等处理
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("=======LogInterceptor.preHandle===============");
+        logger.info("---------LogInterceptor.preHandle---------------");
         return true;
     }
 
+    //后置处理，可以在这里对ModelAndView进行修改
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        super.postHandle(request, response, handler, modelAndView);
+        logger.info("---------LogInterceptor.postHandle---------------");
     }
 
+    //可以根据ex是否为null判断是否发生了异常，进行日志记录或者清理资源
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        super.afterCompletion(request, response, handler, ex);
+        logger.info("---------LogInterceptor.afterCompletion---------------");
     }
 }
