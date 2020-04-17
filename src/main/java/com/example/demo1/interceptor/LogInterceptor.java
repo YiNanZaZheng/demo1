5f03e47693d386fe60e9d4a8ba9b5e5e4d1a42ad
YINanZaZheng <614843497@qq.com>
2020-04-17 11:24:01 +0800

    测试git

--- src/main/java/com/example/demo1/interceptor/LogInterceptor.java
+++ src/main/java/com/example/demo1/interceptor/LogInterceptor.java
@@ -1,7 +1,10 @@
 package com.example.demo1.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LogInterceptor {
  //  十大
     //准写代码了，能提交吗
-
+    private static final Logger logger= LoggerFactory.getLogger(LogInterceptor.class);
 }
